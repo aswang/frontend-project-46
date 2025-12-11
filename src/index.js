b3abacb262536604d455ce18ec3c3008da606c06
Aswang <testoks@yandex.ru>
2025-12-11 14:32:35 +0300

    Шаг 4. Сравнение плоских файлов (JSON)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+import _ from 'lodash';
 import parseFile from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  console.log('File 1:', data1);
-  console.log('File 2:', data2);
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
 
-  return 'Diff not implemented yet';
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+  const lines = sortedKeys.map((key) => {
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+
+    if (!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+    }
+
+    return `    ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 export default genDiff;
