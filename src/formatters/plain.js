4c932de541cd5f648b499454d14639c8202a0c33
Aswang <testoks@yandex.ru>
2026-04-13 13:26:28 +0300

    Fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,36 +1,36 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 const stringify = (value) => {
   if (_.isPlainObject(value)) {
-    return '[complex value]';
+    return '[complex value]'
   }
   if (typeof value === 'string') {
-    return `'${value}'`;
+    return `'${value}'`
   }
-  return String(value);
-};
+  return String(value)
+}
 
 const formatPlain = (diff, path = '') => {
   const lines = diff
-    .filter((node) => node.type !== 'unchanged')
+    .filter(node => node.type !== 'unchanged')
     .map((node) => {
-      const fullPath = path ? `${path}.${node.key}` : node.key;
+      const fullPath = path ? `${path}.${node.key}` : node.key
 
       switch (node.type) {
         case 'added':
-          return `Property '${fullPath}' was added with value: ${stringify(node.value)}`;
+          return `Property '${fullPath}' was added with value: ${stringify(node.value)}`
         case 'removed':
-          return `Property '${fullPath}' was removed`;
+          return `Property '${fullPath}' was removed`
         case 'changed':
-          return `Property '${fullPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+          return `Property '${fullPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`
         case 'nested':
-          return formatPlain(node.children, fullPath);
+          return formatPlain(node.children, fullPath)
         default:
-          throw new Error(`Unknown node type: ${node.type}`);
+          throw new Error(`Unknown node type: ${node.type}`)
       }
-    });
+    })
 
-  return lines.join('\n');
-};
+  return lines.join('\n')
+}
 
-export default formatPlain;
+export default formatPlain
